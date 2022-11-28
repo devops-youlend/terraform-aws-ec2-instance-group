33b90b228a4ed0493d82f8489599fbee7c3d7fc0
Woolersuk <50249037+Woolersuk@users.noreply.github.com>
2022-11-28 14:35:01 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -121,7 +121,7 @@ resource "aws_instance" "default" {
     }
   )
   lifecycle {
-    ignore_changes = [ami, subnet_id, ebs_optimized, hibernation]
+    ignore_changes = [ami, subnet_id]
   }    
 }
 
