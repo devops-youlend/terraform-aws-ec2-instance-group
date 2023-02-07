fd9fd76d7847d67ad0e7212edd349a1abf6e60c8
Woolersuk <50249037+Woolersuk@users.noreply.github.com>
2023-02-07 11:19:29 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -121,7 +121,7 @@ resource "aws_instance" "default" {
     }
   )
   lifecycle {
-    ignore_changes = [ami, subnet_id, ebs_optimized, hibernation, tags, tags_all]
+    ignore_changes = [ami, subnet_id, ebs_optimized, hibernation, tags["ScheduleMessage"], tags_all["ScheduleMessage"]]
   }    
 }
 
