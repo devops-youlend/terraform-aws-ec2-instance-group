079deb2baba73d23ebbfd5789db1daab2a6b89a3
Woolersuk <50249037+Woolersuk@users.noreply.github.com>
2023-05-26 10:29:44 +0100

    Update eni.tf
    │ Warning: Argument is deprecated
    │
    │   with module.amis.module.app_instance.aws_eip.additional,
    │   on .terraform/modules/amis.app_instance/eni.tf line 24, in resource "aws_eip" "additional":
    │   24:   vpc               = true
    │
    │ use domain attribute instead

--- eni.tf
+++ eni.tf
@@ -21,7 +21,8 @@ resource "aws_network_interface_attachment" "additional" {
 
 resource "aws_eip" "additional" {
   count             = local.additional_ips_count * var.instance_count
-  vpc               = true
+  #vpc               = true
+  domain            = vpc
   network_interface = aws_network_interface.additional.*.id[count.index]
   depends_on        = [aws_instance.default]
 }
